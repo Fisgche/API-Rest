3841a1c659e91f7fbe6c1b65659803bf643ae08e
Fisgche <gustavooliveirafischer@hotmail.com>
2025-09-08 20:21:58 -0300

    adicona routers

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import http from 'http';
 import express from 'express';
+import apiV1Router from './api-v1/api-router.js';
 
 const app = express();
 const port = 5500;
 
-app.all('/', (req, res, next) => res.send('Tudo funcionando'));
+app.use('/api/v1', apiV1Router); // Assuming apiV1Router is defined elsewhere
+app.use('/', (req, res) => res.send('----Api Contatos----'));
 
 http.createServer(app).listen(port, () => {
   console.log('Server is running on http://localhost:' + port);
